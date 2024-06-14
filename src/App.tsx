9e5ffb396b2e438646a53c2b3045f1ae2b254d8a
Kasia <kasia1781@gmail.com>
2024-06-14 21:15:37 +0200

    added Home component and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+
+//https://example.com - to jest domena
+//https://example.com/products - to jest ścieżka
+//https://example.com/ - to też jest ścieżka
+
+const router = createBrowserRouter([
+	{ path: '/', element: <Home /> }, //ścieżka dla strony głównej
+	{},
+]);
 
 function App() {
-	return <p>Hello!</p>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
