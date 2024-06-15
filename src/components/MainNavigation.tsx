4d54f6de1272a5b46c58ce048f7e06fc5cf83637
Kasia <kasia1781@gmail.com>
2024-06-15 18:12:33 +0200

    added Outlet in RootLayout component

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 //pasek nawigacyjny widoczny na wszystkich stronach
 export default function MainNavigation() {
 	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to='/'>Home</Link>
-				</li>
-				<li>
-					<Link to='/products'>Products</Link>
-				</li>
-			</ul>
-		</nav>
+		<header className='header'>
+			<nav>
+				<ul className='list'>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/products'>Products</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 }
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,4 +1,14 @@
 // Dodajemy Layout który zawija wszystkie trasy i ładuje komponenty główne wewnątrz zawijającego komponentu. Dodajemy nową ścieżkę a jako element dodajemy wrapper który powinien byc zawinięty wokół innych tras.
+import { Outlet } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+
 export default function RootLayout() {
-	return <h1>Root Layout</h1>;
+	return (
+		<>
+			<MainNavigation />
+			<main className='content'>
+				<Outlet />
+			</main>
+		</>
+	);
 }
