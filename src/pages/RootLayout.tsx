0dc1d335800994287986d72cdea2b032540f1a7b
Kasia <kasia1781@gmail.com>
2024-06-15 17:59:36 +0200

    creating RootLayout component

---
+++ src/pages/RootLayout.tsx
@@ -0,0 +1,4 @@
+// Dodajemy Layout który zawija wszystkie trasy i ładuje komponenty główne wewnątrz zawijającego komponentu. Dodajemy nową ścieżkę a jako element dodajemy wrapper który powinien byc zawinięty wokół innych tras.
+export default function RootLayout() {
+	return <h1>Root Layout</h1>;
+}
