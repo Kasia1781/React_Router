105f39d635f6db48b8c59be96104bf61cd5eac68
Kasia <kasia1781@gmail.com>
2024-06-15 18:23:35 +0200

    added NavLink in MainNavigation component

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,16 +1,26 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 //pasek nawigacyjny widoczny na wszystkich stronach
+//Używamy NavLink jako zamiennika do Link. Ma on specjalne zachowanie: gdy dodamy klasę to klasa ta przyjmuje funkcję która powinna zwrócić nazwę klasy która powinna byc dodana. Funkcja ta automatycznie otrzymuje obiekt z którego możemy destrukturyzować właściwość isActive który jest dostarczany przez Router i ma wartość boolean. Dodajemy też wlaściwość end do jednego z linków aby wskazać który jest aktywny.
 export default function MainNavigation() {
 	return (
 		<header className='header'>
 			<nav>
 				<ul className='list'>
 					<li>
-						<Link to='/'>Home</Link>
+						<NavLink
+							to='/'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+							end>
+							Home
+						</NavLink>
 					</li>
 					<li>
-						<Link to='/products'>Products</Link>
+						<NavLink
+							to='/products'
+							className={({ isActive }) => (isActive ? 'active' : '')}>
+							Products
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
