2136635cd7663f16cdc8ce8b9afff86c07236afc
Kasia <kasia1781@gmail.com>
2024-06-15 18:38:46 +0200

    added useParams hook

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+//Używamy hook useParams który daje nam obiekt który zawiera dynamiczny segment ścieżki zdefiniowanej w korzeniu jako właściwość.
 export default function ProductDetail() {
+	const params = useParams();
+	console.log(params);
+
 	return (
 		<>
 			<h1>Product Details!</h1>
+			<p>{params.productId}</p>
 		</>
 	);
 }
