5373b87e0807011db0fa8dcb44d3f9f2bba756a1
Kasia <kasia1781@gmail.com>
2024-06-15 18:15:40 +0200

    creating ErrorPage component and added in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import RootLayout from './pages/RootLayout';
+import ErrorPage from './pages/ErrorPage';
 
 //https://example.com - to jest domena
 //https://example.com/products - to jest ścieżka
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <RootLayout />,
+		errorElement: <ErrorPage />,
 		children: [
 			{ path: '/', element: <Home /> }, //ścieżka dla strony głównej
 			{ path: '/products', element: <Products /> },
