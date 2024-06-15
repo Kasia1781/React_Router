684cdd21354530138ab4f91f7b92428bfd8070b6
Kasia <kasia1781@gmail.com>
2024-06-15 18:44:43 +0200

    added Link

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,22 @@
+import { Link } from 'react-router-dom';
+
+//Używamy hook useParams który daje nam obiekt który zawiera dynamiczny segment ścieżki zdefiniowanej w korzeniu jako właściwość.
+const PRODUCTS = [
+	{ id: 'p1', title: 'Product 1' },
+	{ id: 'p2', title: 'Product 2' },
+	{ id: 'p3', title: 'Product 3' },
+];
+
 export default function Products() {
 	return (
 		<>
 			<h1>The Products Page</h1>
 			<ul>
-				<li>Product 1</li>
-				<li>Product 2</li>
-				<li>Product 3</li>
+				{PRODUCTS.map((product) => (
+					<li key={product.id}>
+						<Link to={`/products/${product.id}`}>{product.title}</Link>
+					</li>
+				))}
 			</ul>
 		</>
 	);
